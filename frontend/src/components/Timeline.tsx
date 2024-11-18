09afcc232fab7a43107657595cefe44923a3328f
chirag-ghosh <cghosh828049@gmail.com>
2024-11-18 22:41:50 +0530

    feat: add infinite scrolling
    Signed-off-by: chirag-ghosh <cghosh828049@gmail.com>

--- frontend/src/components/Timeline.tsx
+++ frontend/src/components/Timeline.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { IPhoto } from "../types/Photo";
 import { BACKEND_URL } from "../constants";
 
@@ -9,14 +9,55 @@ const getFormattedDate = (rawDate: Date) => {
 
 function Timeline() {
     const [photos, setPhotos] = useState<IPhoto[]>([]);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+    const [hasNextPage, setHasNextPage] = useState(true);
+
+    const observerTarget = useRef(null);
+
+    const fetchPhotos = async () => {
+        if(loading || !hasNextPage) return;
+        setLoading(true);
+
+        try {
+            const response = await fetch(`${BACKEND_URL}/timeline?page=${page}`)
+            const data = await response.json()
+
+            if(data == null || data.length === 0) {
+                setHasNextPage(false)
+            } else {
+                setPhotos(prevPhotos => [...prevPhotos, ...data])
+                setPage(prevPage => prevPage + 1)
+            }
+        } catch(error) {
+            console.log(error)
+        } finally {
+            setLoading(false)
+        }
+    }
 
     useEffect(() => {
-        fetch(`${BACKEND_URL}/timeline`)
-            .then((res) => res.json())
-            .then((data) => {
-                setPhotos(data);
+        const observe = new IntersectionObserver((entries) => {
+            console.log(entries);
+            entries.forEach(entry => {
+                if(entry.intersectionRatio > 0 && entry.intersectionRatio <= 1) {
+                    fetchPhotos()
+                }
             })
-    }, []);
+        }, {
+            rootMargin: "0px 0px 200px 0px"
+        })
+
+        if(observerTarget.current) {
+            observe.observe(observerTarget.current);
+        }
+
+        return () => {
+            if(observerTarget.current) {
+                observe.unobserve(observerTarget.current);
+            }
+        }
+    }, [observerTarget, photos, page, loading, hasNextPage]);
 
     return (
         <div className='timeline'>
@@ -29,6 +70,8 @@ function Timeline() {
                     </div>
                 </div>
             ))}
+            {loading && hasNextPage && <p className="loading">Loading...</p>}
+            <div ref={observerTarget}></div>
         </div>
     );
 }
